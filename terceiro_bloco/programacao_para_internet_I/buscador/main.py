18fc648e7bb591b600077f7ff0b6438c7ac76015
Isaias Santana Oliveira <isaiassantana304@gmail.com>
2023-03-03 14:54:24 -0300

    correcoes

--- terceiro_bloco/programacao_para_internet_I/buscador/main.py
+++ terceiro_bloco/programacao_para_internet_I/buscador/main.py
@@ -1,9 +1,15 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
+from helpers.word import Word
 from models.pages import Pages
 
 pages = Pages()
+words = Word()
+
+# pages.drop()
+# pages.table()
 
 page: str = input('Page: ')
 word: str = input('Word: ')
@@ -14,41 +20,54 @@ rpf = pages.filter(
     word=word,
     depth=depth)
 if len(rpf) > 0:
-    print("#"*100, "Já existe")
+    print("#"*50, "Já existe")
+    print(rpf)
 else:
-    pages.insert(
+    register_id: int = pages.insert(
         page=page,
         word=word,
         depth=depth)
     
     if depth == 0:
-        response = requests.get(page)
-        index = response.text.rfind(word)
-        print(response.text[index])
+        r = requests.get(page)
+        phrase = words.get_text(
+            text=r.text,
+            word=word)
+        pages.update_by_id(
+            id=register_id,
+            phrase=phrase)
+        
     elif depth == 1:
         response = requests.get(page)
         html = BeautifulSoup(response.text, 'html5lib')
         links = html.find_all('a')
-        ranking = {}
         for link in links:
-            print(link.get('href'))
-        #     if link.get('href'):
-        #         split = link['href'].split('https://g1.globo.com/')
-        #         if len(split) > 1:
-        #             if split[1].split('/')[0] not in ranking:
-        #                 ranking[split[1].split('/')[0]]=1
-        #             else:
-        #                 ranking[split[1].split('/')[0]]+=1
-
-        # print(ranking)
+            if 'https' in link.get('href'):
+                r = requests.get(link.get('href'))
+                phrase = words.get_text(
+                    text=r.text,
+                    word=word)
+                pages.update_by_id(
+                    id=register_id,
+                    phrase=phrase)
+                print(phrase)
+    
     elif depth == 2:
         response = requests.get(page)
         html = BeautifulSoup(response.text, 'html5lib')
         links = html.find_all('a')
-        ranking = {}
         for link in links:
-            response = requests.get(link.get('href'))
-            html = BeautifulSoup(response.text, 'html5lib')
-            links2 = html.find_all('a')
-            for l in links2:
-                print(l.get('href'))
+            if 'https' in link.get('href'):
+                response = requests.get(link.get('href'))
+                html = BeautifulSoup(response.text, 'html5lib')
+                links2 = html.find_all('a')
+                for l in links2:
+                    if 'https' in l.get('href'):
+                        r = requests.get(l.get('href'))
+                        phrase = words.get_text(
+                            text=r.text,
+                            word=word)
+                        pages.update_by_id(
+                            id=register_id,
+                            phrase=phrase)
+                        print(phrase)
--- terceiro_bloco/programacao_para_internet_I/buscador/models/pages.py
+++ terceiro_bloco/programacao_para_internet_I/buscador/models/pages.py
@@ -2,13 +2,15 @@ from config.postgres import Models
 
 
 class Pages(Models):
-    def insert(self, page: str, word: str, depth: str) -> dict:
-        query = '''INSERT INTO public.pages (page, word, depth) VALUES ('{}', '{}', '{}')'''.format(
+    def insert(self, page: str, word: str, depth: str) -> int:
+        query = '''
+            INSERT INTO public.pages (page, word, depth) VALUES ('{}', '{}', '{}') RETURNING id;'''.format(
             page,
             word,
             depth)
         self.cur.execute(query)
         self.con.commit()
+        return self.cur.fetchone()[0]
 
     
     def filter(self, page: str, word: str, depth: str) -> list:
@@ -17,7 +19,7 @@ class Pages(Models):
             word,
             depth)
         self.cur.execute(query)
-        return [{'page': p[0], 'word': p[1], 'depth': p[2]} for p in self.cur.fetchall()]
+        return [{'page': p[1], 'word': p[2], 'depth': p[3], "phrase": p[4]} for p in self.cur.fetchall()]
     
 
     def all(self) -> list:
@@ -26,7 +28,18 @@ class Pages(Models):
         return self.cur.fetchall()
 
 
-    def drop(self)-> bool:
+    def update_by_id(self, id: int, phrase: str) -> bool:
+        try:
+            query = "UPDATE public.pages SET phrase='{}' WHERE id={}".format(
+                phrase,
+                id)
+            self.cur.execute(query)
+            self.con.commit()
+            return True
+        except:
+            return False
+
+    def drop(self) -> bool:
         try:
             query = '''DROP TABLE public.pages'''
             self.cur.execute(query)
@@ -35,7 +48,7 @@ class Pages(Models):
         except:
             return False
 
-    def table(self)-> bool:
+    def table(self) -> bool:
         try:
             query = '''
                     CREATE TABLE IF NOT EXISTS public.pages (
@@ -43,6 +56,7 @@ class Pages(Models):
                     page text,
                     word text,
                     depth text,
+                    phrase text null,
                     created timestamp without time zone NOT NULL DEFAULT now()
                 )
             '''
