e19496151bf8170c20dde4862dc2879920d1d477
Isaias Santana Oliveira <isaiassantana304@gmail.com>
2023-03-03 11:37:12 -0300

    feat: Melhorias no projeto

--- terceiro_bloco/programacao_para_internet_I/buscador/main.py
+++ terceiro_bloco/programacao_para_internet_I/buscador/main.py
@@ -3,14 +3,52 @@ from bs4 import BeautifulSoup
 
 from models.pages import Pages
 
+pages = Pages()
 
-site: str = input('Site de pesquisa: ')
-Pages().insert(url=site)
-response = requests.get(site)
+page: str = input('Page: ')
+word: str = input('Word: ')
+depth: int = int(input('Depth: '))
 
-palavra = input('Pesquise aqui: ')
-index = response.text.rfind(palavra)
-print(response.text[index])
+rpf = pages.filter(
+    page=page,
+    word=word,
+    depth=depth)
+if len(rpf) > 0:
+    print("#"*100, "Já existe")
+else:
+    pages.insert(
+        page=page,
+        word=word,
+        depth=depth)
+    
+    if depth == 0:
+        response = requests.get(page)
+        index = response.text.rfind(word)
+        print(response.text[index])
+    elif depth == 1:
+        response = requests.get(page)
+        html = BeautifulSoup(response.text, 'html5lib')
+        links = html.find_all('a')
+        ranking = {}
+        for link in links:
+            print(link.get('href'))
+        #     if link.get('href'):
+        #         split = link['href'].split('https://g1.globo.com/')
+        #         if len(split) > 1:
+        #             if split[1].split('/')[0] not in ranking:
+        #                 ranking[split[1].split('/')[0]]=1
+        #             else:
+        #                 ranking[split[1].split('/')[0]]+=1
 
-html = BeautifulSoup(response.text, 'html5lib')
-links = html.find_all('a')
+        # print(ranking)
+    elif depth == 2:
+        response = requests.get(page)
+        html = BeautifulSoup(response.text, 'html5lib')
+        links = html.find_all('a')
+        ranking = {}
+        for link in links:
+            response = requests.get(link.get('href'))
+            html = BeautifulSoup(response.text, 'html5lib')
+            links2 = html.find_all('a')
+            for l in links2:
+                print(l.get('href'))
--- terceiro_bloco/programacao_para_internet_I/buscador/models/pages.py
+++ terceiro_bloco/programacao_para_internet_I/buscador/models/pages.py
@@ -2,13 +2,52 @@ from config.postgres import Models
 
 
 class Pages(Models):
-    def insert(self, url: str) -> dict:
-        query = '''INSERT INTO public.pages (page) VALUES ('{}')'''.format(url)
+    def insert(self, page: str, word: str, depth: str) -> dict:
+        query = '''INSERT INTO public.pages (page, word, depth) VALUES ('{}', '{}', '{}')'''.format(
+            page,
+            word,
+            depth)
         self.cur.execute(query)
         self.con.commit()
 
+    
+    def filter(self, page: str, word: str, depth: str) -> list:
+        query = "SELECT * FROM public.pages WHERE page='{}' AND word='{}' AND depth='{}'".format(
+            page,
+            word,
+            depth)
+        self.cur.execute(query)
+        return [{'page': p[0], 'word': p[1], 'depth': p[2]} for p in self.cur.fetchall()]
+    
 
     def all(self) -> list:
         query = '''SELECT * FROM public.pages'''
         self.cur.execute(query)
-        return cur.fetchall()
+        return self.cur.fetchall()
+
+
+    def drop(self)-> bool:
+        try:
+            query = '''DROP TABLE public.pages'''
+            self.cur.execute(query)
+            self.con.commit()
+            return True
+        except:
+            return False
+
+    def table(self)-> bool:
+        try:
+            query = '''
+                    CREATE TABLE IF NOT EXISTS public.pages (
+                    id serial NOT NULL,
+                    page text,
+                    word text,
+                    depth text,
+                    created timestamp without time zone NOT NULL DEFAULT now()
+                )
+            '''
+            self.cur.execute(query)
+            self.con.commit()
+            return True
+        except:
+            return False
